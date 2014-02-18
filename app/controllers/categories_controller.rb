56f0496a74fb914b381ed8c9eca5aaa27dd39ead
Thiago Velozo <thiagovelozo2@gmail.com>
2014-02-18 22:54:54 +0000

    added create categories feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,4 +8,21 @@ class CategoriesController < ApplicationController
 		@category = Category.find(params[:id])
 	end
 
+	def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category
+    else
+      render "new"
+    end
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
